3f0e51b6d3f93da3520260824d324b99e1584c7d
pbj <pbj@pop-os.localdomain>
2019-01-27 11:10:06 -0500

    Testing xmlhttprequests

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ function submitQuery() {
   //alert(queryResult);
 
   //Code Block used to Change Color=Red and Text=Not Working
-  // document.getElementById("result").className = "text-red text-uppercase";
-  // document.getElementById("result").innerHTML = "NOT WORKING";
+
+  
+   document.getElementById("result").className = "text-red text-uppercase";
+   document.getElementById("result").innerHTML = "NOT WORKING";
 
   //Code Block used to Change Color=Green /text-primary and Text=Working
   //document.getElementById("result").className = "text-primary text-uppercase"
@@ -17,8 +19,23 @@ function submitQuery() {
     console.log(evt);
   }, false);
 
-  request.open('GET', 'https://127.0.0.1:5000/0002', true),
+  request.open('GET', 'https://127.0.0.1:5000/', true),
   request.send();
+
+  var xhttp = new XMLHttpRequest();
+  
+  const xhr = new XMLHttpRequest()
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+      xhr.status === 200 ? console.log(xhr.responseText) : console.error('error')
+    }
+  }
+  
+  xhr.open("GET", "http://127.0.0.1:5000/0002");
+  xhr.send();
+
+
+
   //  var hello = request.response;
   //  alert(hello);
 }
